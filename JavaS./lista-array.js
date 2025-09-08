3f74e663cd4886d4fbf4f73fffd9142191a97ce0
lc22052008 <168545216+lc22052008@users.noreply.github.com>
2025-09-08 11:33:00 -0300

    Delete JavaS. directory

--- JavaS./lista-array.js
+++
@@ -1,4 +0,0 @@
-const listaCPFs = [1111111, 22222, 3333];
-const informacoesPessoa = ["nome", "Lucas", "idade", 32, "CPF", "1111222333"];
-console.log(informacoesPessoa [1]);
-console.log(listaCPFs [2])
