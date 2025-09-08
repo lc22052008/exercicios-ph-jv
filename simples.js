c6f95f12ba26a2751d4fbc4f95ceaaa8fd6e0bde
lc22052008 <168545216+lc22052008@users.noreply.github.com>
2025-09-08 08:21:03 -0300

    Add files via upload

---
+++ simples.js
@@ -0,0 +1,10 @@
+var pais = "Brasil"
+
+console.log (`Você esta morando no(a) ${pais}`)
+
+if (pais=="Brasil"){
+    console.log ("Você é brasileiro!!")
+}
+else {
+    console.log ("Você é estrangeiro!!")
+}
---
+++ teste.js
@@ -0,0 +1,9 @@
+   
+        function soma() {
+            var n1 = Number(window.prompt('Digite um número: '));
+            var n2 = Number(window.prompt('Digite outro número:'));
+            let = n1 + n2
+
+            var resultado = document.querySelector('section#ressultado');
+            var resultado.innerHTML = A soma entre ${n1}e ${n2}é igual a ${'somando'};
+        }
