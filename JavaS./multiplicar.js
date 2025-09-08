97e7e2934e108e2a1506bcd72b20af71e92921c6
lc22052008 <168545216+lc22052008@users.noreply.github.com>
2025-09-08 08:49:13 -0300

    Add files via upload

---
+++ JavaS./multiplicar.js
@@ -0,0 +1,10 @@
+function multiplicar() {
+    const num1 = parseFloat
+    (document.getElementById("numero1").value);
+
+    const num2 = parseFloat
+    (document.getElementById("numero2").value); 
+
+    const soma = num1 * num2;    
+    document.getElementById("resultado").textContent = "A soma é: " + soma;
+}
---
+++ JavaS./teste-comand copy 4.js
@@ -0,0 +1,16 @@
+n=70
+if (n>=200) {
+true
+{
+        window.alert("O valor de N é MAIOR que 200!")
+}
+} 
+else
+
+if (n>=100) {
+    window.alert('O valor de N é MAIOR que 100!')
+} 
+else 
+{
+    window.alert('O valor de N é MENOR que 100!')
+}
