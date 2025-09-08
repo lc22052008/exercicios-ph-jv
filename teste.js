5fe6869fcc6e0b01a561ad899a49db106d29d9e1
lc22052008 <168545216+lc22052008@users.noreply.github.com>
2025-09-08 08:55:13 -0300

    Delete teste.js

--- teste.js
+++
@@ -1,9 +0,0 @@
-   
-        function soma() {
-            var n1 = Number(window.prompt('Digite um número: '));
-            var n2 = Number(window.prompt('Digite outro número:'));
-            let = n1 + n2
-
-            var resultado = document.querySelector('section#ressultado');
-            var resultado.innerHTML = A soma entre ${n1}e ${n2}é igual a ${'somando'};
-        }
